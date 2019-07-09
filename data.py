dbd162acb0f0da9d8d607c2e5c83c357bacf746c
TkhiienLok <tkhiien@gmail.com>
2019-07-09 18:10:30 +0300

    Added home, about, articles pages and registration form

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id tortor id dui elementum luctus quis et lacus. Donec eleifend accumsan augue ut feugiat. Nunc quis consectetur sapien, nec ultrices nulla. Ut pretium vitae dui sed sollicitudin. Suspendisse vestibulum non nunc commodo tincidunt. Phasellus ut tortor blandit sapien ultricies dapibus ac at tortor. Donec viverra dui ac fermentum aliquet. Proin non facilisis augue. Sed et vestibulum dolor. Maecenas sed tortor eu ipsum porta facilisis. In hac habitasse platea dictumst. Donec ut nisi nec nulla venenatis tincidunt vel et orci. Mauris molestie justo metus, in molestie nibh semper et. Vestibulum eget facilisis ipsum.',
+            'author':'Ivan Ivanov',
+            'create date':'07-08-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id tortor id dui elementum luctus quis et lacus. Donec eleifend accumsan augue ut feugiat. Nunc quis consectetur sapien, nec ultrices nulla. Ut pretium vitae dui sed sollicitudin. Suspendisse vestibulum non nunc commodo tincidunt. Phasellus ut tortor blandit sapien ultricies dapibus ac at tortor. Donec viverra dui ac fermentum aliquet. Proin non facilisis augue. Sed et vestibulum dolor. Maecenas sed tortor eu ipsum porta facilisis. In hac habitasse platea dictumst. Donec ut nisi nec nulla venenatis tincidunt vel et orci. Mauris molestie justo metus, in molestie nibh semper et. Vestibulum eget facilisis ipsum.',
+            'author': 'John Doe',
+            'create date': '07-08-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id tortor id dui elementum luctus quis et lacus. Donec eleifend accumsan augue ut feugiat. Nunc quis consectetur sapien, nec ultrices nulla. Ut pretium vitae dui sed sollicitudin. Suspendisse vestibulum non nunc commodo tincidunt. Phasellus ut tortor blandit sapien ultricies dapibus ac at tortor. Donec viverra dui ac fermentum aliquet. Proin non facilisis augue. Sed et vestibulum dolor. Maecenas sed tortor eu ipsum porta facilisis. In hac habitasse platea dictumst. Donec ut nisi nec nulla venenatis tincidunt vel et orci. Mauris molestie justo metus, in molestie nibh semper et. Vestibulum eget facilisis ipsum.',
+            'author': 'Bob Foster',
+            'create date': '07-08-2019'
+        }
+    ]
+    return articles
